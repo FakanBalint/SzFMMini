5e32d49d3824a2587e504fda070d61c76bdf7ec6
FakanBalint <fakan.balint@gmail.com>
2023-09-23 18:53:19 +0200

    login and register page's Rough shpae is made

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-
+import 'bootstrap/dist/css/bootstrap.css';
+import Registerpage from './Pages/Registerpage.vue';
 </script>
 
 <template>
-  <h1></h1>
+  <div class="container">
+    <h1>Hello, Bootstrap!</h1>
+    <Registerpage />
+    
+  </div>
 </template>
 
 <style scoped>
---
+++ src/Pages/Loginpage.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+  
+    <div class="header">
+        <h1>Login Page</h1>
+    </div>
+    <div>
+        <input type="text" placeholder="Username">
+        <input type="text" placeholder="Password">
+
+        
+    </div>
+
+        <div>
+        <!-- Bootstrap Grid Container -->
+        <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <!-- Button 1 -->
+            <button class="btn btn-primary">Login</button>
+            </div>
+            <div class="col-md-6">
+            <!-- Button 2 -->
+            <button class="btn btn-secondary">Dont Have an account make one !</button>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+  
+</template>
+
+<style scoped>
+
+</style>
